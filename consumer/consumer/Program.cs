95fece522c621bc0c15f32e9006d672d6c912e77
User <User@DESKTOP-0QGLCAB>
2024-06-07 12:52:24 +0400

    first commit

---
+++ consumer/consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+
+string topic = "test-topic";
+
+Console.WriteLine("Consumer 1");
+ConsumerConfig config = new()
+{
+    BootstrapServers = "192.168.101.36:9092",
+    GroupId = "gr1",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe(topic);
+
+
+while (true)
+{
+    ConsumeResult<string,string> result = consumer.Consume();
+    Thread.Sleep(2);
+    Console.WriteLine($"{result.Offset} | {result.Partition.Value} | {result.Value}");
+}
+
---
+++ producer/producer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+string topic = "test-topic";
+
+ProducerConfig config = new()
+{
+    BootstrapServers = "192.168.101.36:9092"
+};
+
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    while (true)
+    {
+        Console.Write("Enter your message: ");
+        var messageValue = Console.ReadLine();
+
+        TopicPartition topicPartition = new TopicPartition(topic, new Partition(new Random().Next(3)));
+
+        Message<Null, string> message = new()
+        {
+            Value = messageValue
+        };
+        var produceResult = await producer.ProduceAsync(topicPartition, message);
+
+        Console.WriteLine($"Sended to {topic} topic");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception occured: {ex.Message}");
+}
+
+
+
+async Task CreateTopic(string name, int partitioCount)
+{
+
+    using var adminClient = new AdminClientBuilder(config).Build();
+    TopicSpecification topicSettings = new()
+    {
+        Name = name,
+        NumPartitions = partitioCount,
+        ReplicationFactor = 1
+    };
+
+    await adminClient.CreateTopicsAsync(new List<TopicSpecification> { topicSettings });
+}
+
